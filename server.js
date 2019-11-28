433cd90b2967e980ed019ca2c220966ca3f92221
Josue Arronis <cruzjos94@gmail.com>
2019-11-28 13:06:01 -0600

    User models, JWT, Middlewares

--- server.js
+++ server.js
@@ -6,20 +6,23 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
-    res.json({msg: 'Welcome to Contact Keeper API...'})
+    res.json({ msg: 'Welcome to Contact Keeper API...' })
 })
 
 // DEFINE ROUTES
 
-app.use('api/users', require('./routes/users'));
-app.use('api/auth', require('./routes/auth'));
-app.use('api/contacts', require('./routes/contacts'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)});
+app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) });
 
 
 
